f577b376fbadc3f86f977bf8993a1556f9c6c75b
larsjuvik <77640590+larsjuvik@users.noreply.github.com>
2021-07-28 15:08:03 +0200

    Added simple cell

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,57 @@
 use macroquad::prelude::*;
 
+const WIDTH: u32 = 1000;
+const HEIGHT: u32 = 600;
+
 fn config() -> Conf {
     Conf {
         window_title: "Game of Life".to_owned(),
-        window_width: 1000,
-        window_height: 600,
+        window_width: WIDTH as i32,
+        window_height: HEIGHT as i32,
         window_resizable: false,
         ..Default::default()
     }
 }
 
+/////////////////
+// CELL STUFF  //
+/////////////////
+type Dimension = (f32, f32);
+enum CellState {
+    DEAD,
+    ALIVE,
+}
+struct Cell {
+    grid_x: u32,
+    grid_y: u32,
+    state: CellState,
+}
+impl Cell {
+    fn new(grid_x: u32, grid_y: u32, state: CellState) -> Cell {
+        Cell { grid_x, grid_y, state }
+    }
+
+    fn draw(&self, size_px: Dimension) {
+        draw_rectangle(
+            self.grid_x as f32 * size_px.0,
+            self.grid_y as f32 * size_px.1,
+            size_px.0,
+            size_px.1,
+            Color::new(255.0, 0.0, 0.0, 1.0)
+        )
+    }
+}
+
+
 #[macroquad::main(config)]
 async fn main() {
 
+    let c1 = Cell::new(3u32, 4u32, CellState::DEAD);
+
     loop {
         clear_background(WHITE);
+
+        c1.draw((60.0, 60.0));
 
         next_frame().await
     }
